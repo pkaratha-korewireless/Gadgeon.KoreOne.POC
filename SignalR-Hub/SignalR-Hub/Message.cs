4f4c2624f8c579338dd0c43908121fd21a4c0161
MK Noormuhammed <mnoormuhammed@korewireless.com>
2019-02-05 15:37:42 +0530

    Added api for SignalR

---
+++ SignalR-Hub/SignalR-Hub/Message.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SignalRHub
+{
+    public class Message
+    {
+        public string id { get; set; }
+        public string imei { get; set; }
+        public DateTime actual_date { get; set; }
+        public decimal latitude { get; set; }
+        public decimal longitude { get; set; }
+        public decimal direction { get; set; }
+        public decimal odometer { get; set; }
+        public decimal speed { get; set; }
+        public decimal temperature { get; set; }
+        public decimal fuel { get; set; }
+        public decimal voltage { get; set; }
+    }
+    
+}
